07d761d5519c69876ed3f92c9123e59f7efb1c3c
sua0109 <127822054+sua0109@users.noreply.github.com>
2023-12-06 02:48:50 +0900

    Add files via upload
    1. privateChat
    2. Role 기능

---
+++ MafiaGame/src/MafiaGame/GameState_Night.java
@@ -0,0 +1,52 @@
+package MafiaGame;
+
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameState_Night extends GameState {
+	String state = "밤";
+	private Timer timer=new Timer();
+	int time = 60;
+	
+	HashMap<String, String> atackList = new HashMap<>();
+	
+	GameState_Night() {
+		super();
+		Mafia_Integrated.broadcastingSystem(time+"초 남았습니다.");
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+            	if(time==30)
+            		Mafia_Integrated.broadcastingSystem(time+"초 남았습니다.");
+            	else if(time==10)
+            		Mafia_Integrated.broadcastingSystem(time+"초 남았습니다.");
+            	else if(time==0)
+            		nextState();
+            	time--;
+            }
+        }, 0, 1000); // 0초부터 시작하여 1초 간격으로 실행
+		
+	}
+
+	String resultAbility(String nickname, String message) {
+		String returnMessage = Mafia.roles.get(nickname).ability(message);
+		return returnMessage;
+	}
+
+	void readMessage(ChatMsg msg) {
+		Mafia_Integrated.players.get(msg.nickname).sendSystemMessageToClient("주무실 시간입니다.");
+	}
+
+	
+	@Override
+	void notifyCreation() {
+		Mafia.broadcastingToAlive(new ChatMsg(ChatMsg.CODE_NIGHT));
+		Mafia_Integrated.broadcastingSystem("밤이 되었습니다.");
+	}
+
+	@Override
+	void nextState() {
+		Mafia.state = new GameState_Day();
+	}
+}
